2cc41141b37ce427b89a64917b6992ef1745534d
Christopher Bruns <cmbruns@rotatingpenguin.com>
2018-05-05 14:41:48 -0700

    Properly save and restore window size. Tidy methods in main_window.py.

--- glcube/main_window.py
+++ glcube/main_window.py
@@ -12,7 +12,6 @@ import glcube
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         QMainWindow.__init__(self, *args, **kwargs)
-        self.readSettings()
         self._setup_ui()
         self.camera = wiggle.PerspectiveCamera()
         self.camera.focus = (0, 1.2, 0)
@@ -22,19 +21,9 @@ class MainWindow(QMainWindow):
         self.multisample_count = self.sampleCountSpinBox.value()
         self.b_multisample = self.multisampleCheckBox.checkState()
 
-    def _setup_ui(self):
-        uic.loadUi(uifile=pkg_resources.resource_stream('glcube', 'glcube.ui'), baseinstance=self)
-        self.openGLWidget.main_window = self
-        self.actionQuit.triggered.connect(self._quit)
-        self.actionMultisample.toggled.connect(self.toggle_multisampling)
-        self.sampleCountSpinBox.valueChanged.connect(self.set_multisample_count)
-        self.actionWireframe.toggled.connect(self.toggle_wireframe)
-
-    def _setup_canvas(self):
-        self.openGLWidget.camera = self.camera
-        self.openGLWidget.renderer = self.renderer
-
-    def _replace_canvas(self, new_canvas):
+    def _change_multisampling(self, sample_count):
+        """Creates a new instance of SceneViewer, to support interactive changing of multisampling parameters"""
+        new_canvas = glcube.scene_canvas.SceneCanvas(self.sceneContainer, samples=sample_count)
         old_canvas = self.openGLWidget
         self.sceneContainer.layout().removeWidget(old_canvas)
         old_canvas.setParent(None)
@@ -42,38 +31,44 @@ class MainWindow(QMainWindow):
         self.openGLWidget = new_canvas
         old_canvas.deleteLater()
         self._setup_canvas()
+        QCoreApplication.processEvents()  # required for repaint below to take
+        self.openGLWidget.repaint()
 
     def _quit(self):
-        # self.closeEvent(None)
-        self.saveSettings()
+        self.save_settings()
         QCoreApplication.quit()
 
+    def _setup_ui(self):
+        uic.loadUi(uifile=pkg_resources.resource_stream('glcube', 'glcube.ui'), baseinstance=self)
+        self.openGLWidget.main_window = self
+        self.actionQuit.triggered.connect(self._quit)
+        self.actionMultisample.toggled.connect(self.toggle_multisampling)
+        self.sampleCountSpinBox.valueChanged.connect(self.set_multisample_count)
+        self.actionWireframe.toggled.connect(self.toggle_wireframe)
+        self.read_settings()
+
+    def _setup_canvas(self):
+        self.openGLWidget.camera = self.camera
+        self.openGLWidget.renderer = self.renderer
+
     def closeEvent(self, event):
-        self.saveSettings()
+        self.save_settings()
         super().closeEvent(event)
 
-    def readSettings(self):
+    def read_settings(self):
         settings = QSettings('Brunsgen International', 'glcube')
         self.restoreGeometry(settings.value('geometry'))
         self.restoreState(settings.value('windowState'))
-        print('readSettings')
 
-    def saveSettings(self):
+    def save_settings(self):
         settings = QSettings('Brunsgen International', 'glcube')
         settings.setValue('geometry', self.saveGeometry())
         settings.setValue('windowState', self.saveState())
-        print('saveSettings')
 
     def set_multisample_count(self, sample_count):
         self.multisample_count = sample_count
         if self.b_multisample:
             self._change_multisampling(sample_count)
-
-    def _change_multisampling(self, sample_count):
-        new_canvas = glcube.scene_canvas.SceneCanvas(self.sceneContainer, samples=sample_count)
-        self._replace_canvas(new_canvas)
-        QCoreApplication.processEvents()  # required for repaint below to take
-        self.openGLWidget.repaint()
 
     def toggle_multisampling(self, checked):
         self.b_multisample = checked

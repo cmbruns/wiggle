bb525f3370f8c38c14c61472504f72afebe34565
Christopher Bruns <cmbruns@rotatingpenguin.com>
2018-05-20 11:16:12 -0700

    Use the exact same test scene in the vr app.

--- scripts/wiggle_vr.py
+++ scripts/wiggle_vr.py
@@ -1,15 +1,11 @@
 from wiggle.render.renderer import Renderer
 import wiggle.render.demo
 from wiggle.app.vr.glfw_vr_app import GlfwVrApp
-from wiggle.render.mesh_actor import PlaneActor
 
 
 def main():
-    # cube = wiggle.render.demo.wireframe_cube_demo()
-    cube = wiggle.render.demo.color_cube_demo()
     renderer = Renderer()
-    renderer.add_actor(cube)
-    renderer.add_actor(PlaneActor())
+    wiggle.render.demo.load_test_scene(renderer)
     with GlfwVrApp(actors=[renderer, ]) as app:
         app.run_loop()
 
